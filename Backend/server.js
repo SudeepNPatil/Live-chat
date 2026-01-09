445675d70bff561f195f87ba15f4334dfe67a0aa
SudeepNPatil <sudeepbsc02@gmail.com>
2026-01-09 12:44:27 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -4,11 +4,12 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
 
 app.use(cors());
 
-dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
@@ -45,6 +46,6 @@ app.use('/', (req, res) => {
   res.send('Server live now, you can chat now');
 });
 
-server.listen(process.env.PORT, () => {
-  console.log(`server is running on http://localhost:${process.env.PORT}`);
+server.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`);
 });
